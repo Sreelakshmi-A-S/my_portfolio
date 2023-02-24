ff6e195962f048aa83ebbbe36a9e5526ac9eb0f2
Sreelakshmi A S <98757905+Sreelakshmi-A-S@users.noreply.github.com>
2023-02-24 12:03:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [" Imaginative", " Developer", " Creative", " Designer", " Content Writer"],
+        strings: [" Content Intern", " Web Designer", " Content Writer", " Team Worker"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [" Imaginative", " Developer", " Creative", " Designer", " Content Writer"],
+        strings: [" Content Intern", " Web Designer", " Content Writer", " Team Worker"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
